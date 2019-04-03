6effe4fcdedc1ff8b6656ef66ed26a377f413277
Your Name <you@example.com>
2019-04-03 20:36:25 +0300

    new data path added

--- src/test/java/com/homeworkqaauto/testcases/PageObjectModel.java
+++ src/test/java/com/homeworkqaauto/testcases/PageObjectModel.java
@@ -12,17 +12,15 @@ public class PageObjectModel {
     private static WebDriver driver = null;
 
     public static void main(String[] args) throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/wowa/Desktop/Java/HomeWork1/src/main/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", ".\\src\\main\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
         BasePage.driver = driver;
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(Constant.HOMEPAGE_URL);
-        Thread.sleep(2000);
         //SignIn.ExecuteLogin(driver);
         SignIn.ExecuteLogin(driver, Constant.VALID_USER_NAME, Constant.VALID_PASSWORD);
-        Thread.sleep(5000);
         SignIn.executelogout(driver);
-        Thread.sleep(2000);
+        //Thread.sleep(2000);
         driver.quit();
         //ExcelDataStorage.setCellData("Passed",1,3);
 
--- src/test/java/com/homeworkqaauto/utils/Constant.java
+++ src/test/java/com/homeworkqaauto/utils/Constant.java
@@ -4,6 +4,6 @@ public interface Constant {
     String HOMEPAGE_URL = "http://automationpractice.com";
     String VALID_USER_NAME = "example@ukr.net";
     String VALID_PASSWORD = "12345678";
-    String PATH_TEST_DATA = "/testdata/";
+    String PATH_TEST_DATA = ".\\src\\test\\java\\com\\homeworkqaauto\\testdata\\";
     String FILENAME_TEST_DATA = "TestData.xlsx";
 }
